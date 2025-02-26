42a2a5fc600950b02f1b24218de22a315eb15158
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-02-26 11:46:25 +0100

    dev: prompt avec image fixe

--- src/requestAI.ts
+++ src/requestAI.ts
@@ -29,7 +29,7 @@ const getOpenAI = () => {
   return openai;
 };
 
-export const requestAI = async (promptValue: string) => {
+export const requestAI = async (promptValue: string): Promise<string> => {
   const openai = getOpenAI();
 
   console.log("start to send request to openai...");
@@ -44,6 +44,7 @@ Tu generes des sites web.
 Tes réponses sont en code HTML seulement.
 Utilises du CSS avec des classes TailwindCSS.
 Ajoute un lien vers le CDN de TailwindCSS.
+Si tu ajoutes des images, utilise toujours le même lien https://cdn.shopify.com/s/files/1/1931/4545/collections/la-photo-de-montagne-paysages-des-alpes-mont-blanc-oisans-647613.jpg?v=1642220795.
 Enleves les lignes avec \`\`\`.
 `,
       },
@@ -57,6 +58,9 @@ Enleves les lignes avec \`\`\`.
 
   console.log(completion.choices[0].message);
   const response = completion.choices[0].message.content;
+  if (!response) {
+    throw new Error("No response from OpenAI.");
+  }
   console.log("response: ", response);
   return response;
 };
