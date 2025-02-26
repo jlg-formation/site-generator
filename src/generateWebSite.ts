35eb224b0ddb48d49ee2cb62309fef24913b02aa
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-02-26 11:21:42 +0100

    dev: manage error if any when generateWebSite

--- src/generateWebSite.ts
+++ src/generateWebSite.ts
@@ -3,13 +3,20 @@ import { requestAI } from "./requestAI";
 export const generateWebSite = () => {
   const form = document.querySelector(".generate-website") as HTMLFormElement;
   form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData.entries());
-    const promptValue =
-      (data.prompt as string) || "Fais moi un site d'agence d'architecte";
-    console.log("promptValue: ", promptValue);
+    try {
+      e.preventDefault();
+      const formData = new FormData(form);
+      const data = Object.fromEntries(formData.entries());
+      const promptValue =
+        (data.prompt as string) || "Fais moi un site d'agence d'architecte";
+      console.log("promptValue: ", promptValue);
 
-    await requestAI(promptValue);
+      await requestAI(promptValue);
+    } catch (err) {
+      console.log("err: ", err);
+      if (err instanceof Error) {
+        alert("Une erreur est survenue : " + err.message);
+      }
+    }
   });
 };
